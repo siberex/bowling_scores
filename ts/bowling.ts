ee3a8058888a90c8ec3c578c79f5a5627eb30459
Stepan Legachev <gitlog@sib.li>
2025-05-01 22:34:43 +0300

    tmp

---
+++ ts/bowling.ts
@@ -0,0 +1,36 @@
+
+enum SCORING {
+    Tenpin_Traditional,
+    Tenpin_IBF,
+    Ninepin,
+    Candlepin,
+    Duckpin,
+    Fivepin,
+};
+
+enum FRAME_TYPE {
+    OPEN,
+    STRIKE,
+    SPARE,
+    SPLIT,
+    GUTTER,
+};
+
+class Frame {
+
+}
+
+class ScoringSheet {
+    scratch: number = 0;
+    handicap = 0;
+    total = 0;
+
+}
+
+class Game {
+    roll(pins: number = 0) {
+
+    };
+
+}
+
